645eb5b21c282b6874a07d7140d07e7227da6812
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-03-29 18:15:17 +0200

    made API endpoint secure

--- src/backend/weatherapi.js
+++ src/backend/weatherapi.js
@@ -1,5 +1,5 @@
 export default async function getWeather(city) {
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2c3727ba2570a1fd7f92097742a63699`);
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2c3727ba2570a1fd7f92097742a63699`);
 
   if (response.status !== 200) {
     throw new Error('Data cannot be fetched');
